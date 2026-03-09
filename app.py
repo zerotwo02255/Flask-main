5c17519f2b85c3a611df66d413c7a22a14fb15c7
zerotwo02255 <ankuwizard5@gmail.com>
2026-03-09 20:07:35 +0545

    hdkjfhds

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///instance/site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
@@ -20,21 +20,27 @@ class jeff(db.Model):
         return f"{self.sno}  -   {self.title}"
     
     
-with app.app_context():
-    db.create_all()
+
 
 @app.route('/')
 def hello_world():
     obj =jeff(title="first to do",desc="indosss")
     db.session.add(obj)
     db.session.commit()
-    return render_template('ankit.html')
+    alljeff=jeff.query.all()
+    return render_template('ankit.html',alljeff=alljeff)
    # return 'hello world!'
 
 
 @app.route('/there')
 def there():
+    alljeff=jeff.query.all()
+    print(alljeff)
     return 'hello there'
 
+with app.app_context():
+    db.create_all()
+    
+    
 if __name__ == "__main__":
     app.run(debug=True,port =9000)
