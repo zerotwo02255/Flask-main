4f6603086eb64ec17a8510f170db62dee89f1c3f
zerotwo02255 <ankuwizard5@gmail.com>
2026-03-14 11:44:16 +0545

    fda

---
+++ flkdsj.py
@@ -0,0 +1,7 @@
+echo "# zerotwo02255" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zerotwo02255/zerotwo02255.git
+git push -u origin main
---
+++ max.py
@@ -0,0 +1,1 @@
+print("tmle pani piyou!!!")
